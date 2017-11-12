fe78422ec798803838caa2f6913ecad04cc4abfc
Ivan Kopenkov <i.kopenkov@rambler-co.ru>
2017-11-12 13:50:21 +0300

    ts added

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,21 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+RRRR to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap RRRR on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: 'Press Cmd+RRRR to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap RRRR on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
-  render() {
+  public render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
@@ -42,7 +30,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'yellow',
+    backgroundColor: 'red',
   },
   welcome: {
     fontSize: 20,
